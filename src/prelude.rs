b6283e3a43045a6632f9289dde40654ba68be5b6
Jason Francis <jafrancis999@gmail.com>
2021-10-26 19:51:13 -0500

    Fix Frame::set_header ownership, prelude trait names

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,6 @@
 #[doc(hidden)]
 pub use gtk::prelude::*;
 
+pub use crate::auto::traits::FrameExt as PanelFrameExt;
+pub use crate::auto::traits::WidgetExt as PanelWidgetExt;
 pub use crate::auto::traits::*;
