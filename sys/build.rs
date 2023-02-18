3baebc7993c619546eaa534c3c57fe9d36c8d39d
Jason Francis <jafrancis999@gmail.com>
2023-02-18 17:58:51 -0500

    fix clippy lint

--- sys/build.rs
+++ sys/build.rs
@@ -4,7 +4,7 @@ fn main() {} // prevent linking libraries to avoid documentation failure
 #[cfg(not(feature = "dox"))]
 fn main() {
     if let Err(s) = system_deps::Config::new().probe() {
-        println!("cargo:warning={}", s);
+        println!("cargo:warning={s}");
         std::process::exit(1);
     }
 }
