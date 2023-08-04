0e9f8fac4b345af305b898f49df88571ad791f99
Bilal Elmoussaoui <belmouss@redhat.com>
2023-08-04 09:36:07 +0200

    Mark adwaita as a requirement
    No idea why it was made optional????
    Also drop not used features, apps should just depend on the right gtk version with the right features

--- src/subclass/mod.rs
+++ src/subclass/mod.rs
@@ -1,7 +1,5 @@
-#[cfg(any(feature = "adw_v1", docsrs))]
 pub mod application;
 pub mod dock;
-#[cfg(any(feature = "adw_v1", docsrs))]
 pub mod document_workspace;
 pub mod frame;
 pub mod frame_header;
@@ -10,14 +8,11 @@ pub mod omni_bar;
 pub mod save_delegate;
 pub mod widget;
 pub mod workbench;
-#[cfg(any(feature = "adw_v1", docsrs))]
 pub mod workspace;
 
 pub mod prelude {
-    #[cfg(any(feature = "adw_v1", docsrs))]
     pub use super::application::PanelApplicationImpl;
     pub use super::dock::{DockImpl, DockImplExt};
-    #[cfg(any(feature = "adw_v1", docsrs))]
     pub use super::document_workspace::{DocumentWorkspaceImpl, DocumentWorkspaceImplExt};
     pub use super::frame::{PanelFrameImpl, PanelFrameImplExt};
     pub use super::frame_header::{FrameHeaderImpl, FrameHeaderImplExt};
@@ -26,7 +21,6 @@ pub mod prelude {
     pub use super::save_delegate::{SaveDelegateImpl, SaveDelegateImplExt};
     pub use super::widget::{PanelWidgetClassSubclassExt, PanelWidgetImpl, PanelWidgetImplExt};
     pub use super::workbench::{WorkbenchClassSubclassExt, WorkbenchImpl, WorkbenchImplExt};
-    #[cfg(any(feature = "adw_v1", docsrs))]
     pub use super::workspace::{WorkspaceClassSubclassExt, WorkspaceImpl};
     pub use gtk::subclass::prelude::*;
 }
